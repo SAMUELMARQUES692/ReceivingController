904e0bbdb9e5473c8daffa0a0dda25a6b9e77a30
samuel <samue@Samuel>
2026-04-20 17:08:56 -0300

    fix: Adiciona uma nova mensagem na classe TicketNotFoundException

--- src/main/java/dev/samuel/ReceivingController/infra/exception/TicketNotFoundException.java
+++ src/main/java/dev/samuel/ReceivingController/infra/exception/TicketNotFoundException.java
@@ -3,6 +3,6 @@ package dev.samuel.ReceivingController.infra.exception;
 public class TicketNotFoundException extends RuntimeException{
 
     public TicketNotFoundException(String ticket) {
-        super("Ticket " + ticket + " não encontrado");
+        super("Ticket " + "'" + ticket + "'" + " não encontrado, tente novamente!!");
     }
 }
